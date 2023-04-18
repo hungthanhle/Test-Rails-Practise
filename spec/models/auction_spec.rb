54475649bda235cd42506fc93f8e58c7ce3365b7
hung <hunglt.ee@gmail.com>
2023-04-18 13:58:42 +0700

    second test

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,14 +1,25 @@
+# https://semaphoreci.com/community/tutorials/how-to-test-rails-models-with-rspec
 require 'rails_helper'
 
 RSpec.describe Auction, :type => :model do
-  it "is valid with valid attributes" do
-    expect(Auction.new).to be_valid
-  end
+  subject { described_class.new }
+
+  # it "is valid with valid attributes" do
+  #   expect(Auction.new).to be_valid
+  # end
 
   it "is not valid without a title" do
     auction = Auction.new(title: nil)
     expect(auction).to_not be_valid
-  end  
+  end
+  
+  it "is valid with valid attributes" do
+    subject.title = "Anything"
+    subject.description = "Anything"
+    subject.start_date = DateTime.now
+    subject.end_date = DateTime.now + 1.week
+    expect(subject).to be_valid
+  end
 
   it "is not valid without a title"
   it "is not valid without a description"
